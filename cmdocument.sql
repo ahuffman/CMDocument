ef60a09fb098a667ee57a37f62e4da65cb768067
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-10-28 13:23:38 -0400

    Added SQL script to genterate requited Postgres DB.

---
+++ cmdocument.sql
@@ -0,0 +1,326 @@
+
+CREATE SEQUENCE public.cmd_disk_raid_type_disk_raid_type_id_seq;
+
+CREATE TABLE public.cmd_disk_raid_type (
+                disk_raid_type_id INTEGER NOT NULL DEFAULT nextval('public.cmd_disk_raid_type_disk_raid_type_id_seq'),
+                disk_raid_type VARCHAR NOT NULL,
+                CONSTRAINT disk_raid_type_id PRIMARY KEY (disk_raid_type_id)
+);
+
+
+ALTER SEQUENCE public.cmd_disk_raid_type_disk_raid_type_id_seq OWNED BY public.cmd_disk_raid_type.disk_raid_type_id;
+
+CREATE SEQUENCE public.cmd_object_obj_id_seq_1;
+
+CREATE TABLE public.cmd_object (
+                obj_id INTEGER NOT NULL DEFAULT nextval('public.cmd_object_obj_id_seq_1'),
+                priority_id INTEGER,
+                owner_id INTEGER,
+                model_id INTEGER,
+                status_id INTEGER,
+                os_id INTEGER,
+                arch_id INTEGER,
+                purpose_id INTEGER,
+                disk_id INTEGER,
+                obj_name VARCHAR NOT NULL,
+                obj_serial VARCHAR NOT NULL,
+                obj_install DATE NOT NULL,
+                obj_decom DATE NOT NULL,
+                obj_ru_size INTEGER NOT NULL,
+                CONSTRAINT obj_id PRIMARY KEY (obj_id)
+);
+
+
+ALTER SEQUENCE public.cmd_object_obj_id_seq_1 OWNED BY public.cmd_object.obj_id;
+
+CREATE TABLE public.cmd_depends (
+                depend_id INTEGER NOT NULL,
+                obj_id INTEGER DEFAULT 1 NOT NULL,
+                obj_id_1 INTEGER NOT NULL,
+                CONSTRAINT depend_id PRIMARY KEY (depend_id, obj_id)
+);
+
+
+CREATE TABLE public.cmd_priority (
+                priority_id INTEGER NOT NULL,
+                priority VARCHAR NOT NULL,
+                CONSTRAINT priority_id PRIMARY KEY (priority_id)
+);
+
+
+CREATE TABLE public.cmd_purpose (
+                purpose_id INTEGER NOT NULL,
+                purpose VARCHAR NOT NULL,
+                CONSTRAINT purpose_id PRIMARY KEY (purpose_id)
+);
+
+
+CREATE TABLE public.cmd_owner (
+                owner_id INTEGER NOT NULL,
+                owner VARCHAR NOT NULL,
+                CONSTRAINT owner_id PRIMARY KEY (owner_id)
+);
+
+
+CREATE SEQUENCE public.cmd_model_model_id_seq;
+
+CREATE TABLE public.cmd_model (
+                model_id INTEGER NOT NULL DEFAULT nextval('public.cmd_model_model_id_seq'),
+                model VARCHAR NOT NULL,
+                vend_id INTEGER NOT NULL,
+                obj_type_id INTEGER NOT NULL,
+                CONSTRAINT model_id PRIMARY KEY (model_id)
+);
+
+
+ALTER SEQUENCE public.cmd_model_model_id_seq OWNED BY public.cmd_model.model_id;
+
+CREATE SEQUENCE public.cmd_status_status_id_seq;
+
+CREATE TABLE public.cmd_status (
+                status_id INTEGER NOT NULL DEFAULT nextval('public.cmd_status_status_id_seq'),
+                status VARCHAR NOT NULL,
+                CONSTRAINT status_id PRIMARY KEY (status_id)
+);
+
+
+ALTER SEQUENCE public.cmd_status_status_id_seq OWNED BY public.cmd_status.status_id;
+
+CREATE SEQUENCE public.cmd_locations_location_id_seq;
+
+CREATE TABLE public.cmd_locations (
+                location_id INTEGER NOT NULL DEFAULT nextval('public.cmd_locations_location_id_seq'),
+                location_name VARCHAR NOT NULL,
+                location_address VARCHAR NOT NULL,
+                CONSTRAINT location_id PRIMARY KEY (location_id)
+);
+
+
+ALTER SEQUENCE public.cmd_locations_location_id_seq OWNED BY public.cmd_locations.location_id;
+
+CREATE SEQUENCE public.cmd_vendors_vend_id_seq;
+
+CREATE TABLE public.cmd_vendors (
+                vend_id INTEGER NOT NULL DEFAULT nextval('public.cmd_vendors_vend_id_seq'),
+                vend VARCHAR NOT NULL,
+                CONSTRAINT vend_id PRIMARY KEY (vend_id)
+);
+
+
+ALTER SEQUENCE public.cmd_vendors_vend_id_seq OWNED BY public.cmd_vendors.vend_id;
+
+CREATE TABLE public.cmd_cab_cont (
+                cab_cont_id INTEGER NOT NULL,
+                cab_id INTEGER NOT NULL,
+                obj_id INTEGER DEFAULT 1 NOT NULL,
+                cab_ru_no INTEGER NOT NULL,
+                CONSTRAINT cab_cont_id PRIMARY KEY (cab_cont_id, cab_id, obj_id)
+);
+
+
+CREATE SEQUENCE public.cmd_cabinets_cab_id_seq;
+
+CREATE TABLE public.cmd_cabinets (
+                cab_id INTEGER NOT NULL DEFAULT nextval('public.cmd_cabinets_cab_id_seq'),
+                location_id INTEGER NOT NULL,
+                cab_name VARCHAR NOT NULL,
+                cab_units_tot INTEGER NOT NULL,
+                cab_image_path VARCHAR,
+                CONSTRAINT cab_id PRIMARY KEY (cab_id)
+);
+
+
+ALTER SEQUENCE public.cmd_cabinets_cab_id_seq OWNED BY public.cmd_cabinets.cab_id;
+
+CREATE SEQUENCE public.cmd_contact_assigned_contact_ass_id_seq;
+
+CREATE TABLE public.cmd_contact_assigned (
+                contact_ass_id INTEGER NOT NULL DEFAULT nextval('public.cmd_contact_assigned_contact_ass_id_seq'),
+                obj_id INTEGER NOT NULL,
+                contact_id INTEGER NOT NULL,
+                CONSTRAINT contact_ass_id PRIMARY KEY (contact_ass_id)
+);
+
+
+ALTER SEQUENCE public.cmd_contact_assigned_contact_ass_id_seq OWNED BY public.cmd_contact_assigned.contact_ass_id;
+
+CREATE SEQUENCE public.cmd_contacts_contact_id_seq;
+
+CREATE TABLE public.cmd_contacts (
+                contact_id INTEGER NOT NULL DEFAULT nextval('public.cmd_contacts_contact_id_seq'),
+                contact_firstname VARCHAR,
+                contact_lastname VARCHAR,
+                contact_company VARCHAR,
+                contact_phone VARCHAR NOT NULL,
+                contact_website VARCHAR,
+                contact_user VARCHAR,
+                contact_pass VARCHAR,
+                contact_email VARCHAR NOT NULL,
+                contact_address VARCHAR,
+                contact_notes VARCHAR,
+                CONSTRAINT contact_id PRIMARY KEY (contact_id)
+);
+
+
+ALTER SEQUENCE public.cmd_contacts_contact_id_seq OWNED BY public.cmd_contacts.contact_id;
+
+CREATE TABLE public.cmd_warranty (
+                warr_id INTEGER NOT NULL,
+                obj_id INTEGER DEFAULT 1 NOT NULL,
+                warr_start DATE NOT NULL,
+                warr_end DATE NOT NULL,
+                CONSTRAINT warr_id PRIMARY KEY (warr_id, obj_id)
+);
+
+
+CREATE SEQUENCE public.cmd_architecture_arch_id_seq;
+
+CREATE TABLE public.cmd_architecture (
+                arch_id INTEGER NOT NULL DEFAULT nextval('public.cmd_architecture_arch_id_seq'),
+                arch VARCHAR NOT NULL,
+                CONSTRAINT arch_id PRIMARY KEY (arch_id)
+);
+
+
+ALTER SEQUENCE public.cmd_architecture_arch_id_seq OWNED BY public.cmd_architecture.arch_id;
+
+CREATE SEQUENCE public.cmd_os_os_id_seq;
+
+CREATE TABLE public.cmd_os (
+                os_id INTEGER NOT NULL DEFAULT nextval('public.cmd_os_os_id_seq'),
+                os VARCHAR NOT NULL,
+                os_ver VARCHAR NOT NULL,
+                arch_id INTEGER,
+                CONSTRAINT os_id PRIMARY KEY (os_id)
+);
+
+
+ALTER SEQUENCE public.cmd_os_os_id_seq OWNED BY public.cmd_os.os_id;
+
+CREATE TABLE public.cmd_ram (
+                ram_id INTEGER NOT NULL,
+                obj_id INTEGER DEFAULT 1 NOT NULL,
+                ram_slot INTEGER NOT NULL,
+                ram_installed VARCHAR NOT NULL,
+                ram_speed VARCHAR NOT NULL,
+                CONSTRAINT ram_id PRIMARY KEY (ram_id, obj_id)
+);
+
+
+CREATE SEQUENCE public.cmd_cpu_type_cpu_type_id_seq;
+
+CREATE TABLE public.cmd_cpu_type (
+                cpu_type_id INTEGER NOT NULL DEFAULT nextval('public.cmd_cpu_type_cpu_type_id_seq'),
+                vend_id INTEGER NOT NULL,
+                cpu_type_model VARCHAR NOT NULL,
+                cpu_type_description VARCHAR NOT NULL,
+                cpu_type_cores INTEGER NOT NULL,
+                CONSTRAINT cpu_type_id PRIMARY KEY (cpu_type_id)
+);
+
+
+ALTER SEQUENCE public.cmd_cpu_type_cpu_type_id_seq OWNED BY public.cmd_cpu_type.cpu_type_id;
+
+CREATE TABLE public.cmd_cpu (
+                cpu_id INTEGER NOT NULL,
+                cpu_type_id INTEGER NOT NULL,
+                obj_id INTEGER NOT NULL,
+                cpu_slot INTEGER NOT NULL,
+                CONSTRAINT cpu_id PRIMARY KEY (cpu_id, cpu_type_id)
+);
+
+
+CREATE TABLE public.cmd_connections (
+                conn_id INTEGER NOT NULL,
+                int_id INTEGER NOT NULL,
+                int_id_1 INTEGER NOT NULL,
+                CONSTRAINT conn_id PRIMARY KEY (conn_id, int_id)
+);
+
+
+CREATE SEQUENCE public.cmd_interface_protocol_int_proto_id_seq;
+
+CREATE TABLE public.cmd_interface_protocol (
+                int_proto_id INTEGER NOT NULL DEFAULT nextval('public.cmd_interface_protocol_int_proto_id_seq'),
+                int_proto VARCHAR NOT NULL,
+                CONSTRAINT int_proto_id PRIMARY KEY (int_proto_id)
+);
+
+
+ALTER SEQUENCE public.cmd_interface_protocol_int_proto_id_seq OWNED BY public.cmd_interface_protocol.int_proto_id;
+
+CREATE SEQUENCE public.cmd_interface_type_int_type_id_seq;
+
+CREATE TABLE public.cmd_interface_type (
+                int_type_id INTEGER NOT NULL DEFAULT nextval('public.cmd_interface_type_int_type_id_seq'),
+                int_type VARCHAR NOT NULL,
+                int_type_desc VARCHAR NOT NULL,
+                CONSTRAINT int_type_id PRIMARY KEY (int_type_id)
+);
+
+
+ALTER SEQUENCE public.cmd_interface_type_int_type_id_seq OWNED BY public.cmd_interface_type.int_type_id;
+
+CREATE TABLE public.cmd_interfaces (
+                int_id INTEGER NOT NULL,
+                int_proto_id INTEGER NOT NULL,
+                int_type_id INTEGER NOT NULL,
+                obj_id INTEGER NOT NULL,
+                int_ip VARCHAR NOT NULL,
+                CONSTRAINT int_id PRIMARY KEY (int_id)
+);
+
+
+CREATE SEQUENCE public.cmd_disk_type_disk_type_id_seq;
+
+CREATE TABLE public.cmd_disk_type (
+                disk_type_id INTEGER NOT NULL DEFAULT nextval('public.cmd_disk_type_disk_type_id_seq'),
+                disk_type VARCHAR NOT NULL,
+                disk_type_desc VARCHAR NOT NULL,
+                CONSTRAINT disk_type_id PRIMARY KEY (disk_type_id)
+);
+
+
+ALTER SEQUENCE public.cmd_disk_type_disk_type_id_seq OWNED BY public.cmd_disk_type.disk_type_id;
+
+CREATE SEQUENCE public.cmd_disk_disk_id_seq;
+
+CREATE TABLE public.cmd_disk (
+                disk_id INTEGER NOT NULL DEFAULT nextval('public.cmd_disk_disk_id_seq'),
+                disk_type_id INTEGER NOT NULL,
+                obj_id INTEGER NOT NULL,
+                disk_raid_type_id INTEGER NOT NULL,
+                disk_physical_count INTEGER NOT NULL,
+                disk_logical_name VARCHAR NOT NULL,
+                disk_logical_to_os_dev VARCHAR NOT NULL,
+                disk_logical_capacity NUMERIC NOT NULL,
+                CONSTRAINT disk_id PRIMARY KEY (disk_id)
+);
+COMMENT ON COLUMN public.cmd_disk.disk_logical_to_os_dev IS 'Device name of the logical disk presented to the OS.';
+
+
+ALTER SEQUENCE public.cmd_disk_disk_id_seq OWNED BY public.cmd_disk.disk_id;
+
+CREATE SEQUENCE public.cmd_object_notes_obj_notes_id_seq;
+
+CREATE TABLE public.cmd_object_notes (
+                obj_notes_id INTEGER NOT NULL DEFAULT nextval('public.cmd_object_notes_obj_notes_id_seq'),
+                obj_id INTEGER NOT NULL,
+                obj_notes VARCHAR NOT NULL,
+                CONSTRAINT obj_notes_id PRIMARY KEY (obj_notes_id)
+);
+
+
+ALTER SEQUENCE public.cmd_object_notes_obj_notes_id_seq OWNED BY public.cmd_object_notes.obj_notes_id;
+
+CREATE SEQUENCE public.cmd_object_type_obj_type_id_seq;
+
+CREATE TABLE public.cmd_object_type (
+                obj_type_id INTEGER NOT NULL DEFAULT nextval('public.cmd_object_type_obj_type_id_seq'),
+                obj_type VARCHAR NOT NULL,
+                obj_type_desc VARCHAR NOT NULL,
+                CONSTRAINT obj_type_id PRIMARY KEY (obj_type_id)
+);
+
+
+ALTER SEQUENCE public.cmd_object_type_obj_type_id_seq OWNED BY public.cmd_object_type.obj_type_id;
