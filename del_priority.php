4528709be2f8b8b24e7c9c45c5a21b1748b6c877
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-11-07 18:15:41 -0500

    fixed bug with extra trailing slash on redirect.

--- del_priority.php
+++ del_priority.php
@@ -4,7 +4,7 @@
       if (isset($_POST['priority_id'])) {$priority_id = pg_escape_string($_POST['priority_id']);}
       if (isset($_POST['priority'])) {$priority = pg_escape_string($_POST['priority']);}
       if (isset($_POST['referer'])) {$ref = pg_escape_string($_POST['referer']);}
-      echo '  <head>' . '<META HTTP-EQUIV=Refresh CONTENT="3;URL=' . $ref . '/">' . '</head>' . '<body>';
+      echo '  <head>' . '<META HTTP-EQUIV=Refresh CONTENT="3;URL=' . $ref . '">' . '</head>' . '<body>';
       if (isset($ref)) {
         $delete = pg_query($db, "DELETE FROM cmd_priority WHERE priority_id=$priority_id");
         if (!$delete) {
