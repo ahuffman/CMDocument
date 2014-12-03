d73c2fc64db7c37d960581318b339a7904ec9b0e
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-12-03 08:51:41 -0500

    Created the delete handler for the cmd_disk_type table linked from the set_hw.html page.

---
+++ del_disk_type.php
@@ -0,0 +1,19 @@
+<html>
+    <?php
+      $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
+      if (isset($_POST['disk_type_id'])) {$disk_type_id = pg_escape_string($_POST['disk_type_id']);}
+      if (isset($_POST['disk_type'])) {$disk_type = pg_escape_string($_POST['disk_type']);}
+      if (isset($_POST['referer'])) {$ref = pg_escape_string($_POST['referer']);}
+      echo '  <head>' . '<META HTTP-EQUIV=Refresh CONTENT="3;URL=' . $ref . '">' . '</head>' . '<body>';
+      if (isset($ref)) {
+        $delete = pg_query($db, "DELETE FROM cmd_disk_type WHERE disk_type_id=$disk_type_id");
+        if (!$delete) {
+        echo 'Delete Failed.';
+        }
+        else {
+        echo 'Deleted ' . $disk_type . ' from the database.' . '<br />';
+        }
+      }
+    ?>
+  </body>
+</html>
