424d926000d003cecc39cb6593a2d1f3edd6cf62
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-12-15 11:04:45 -0500

    Created add action and handler for cpu_type.

--- add_cpu_type.php
+++ add_cpu_type.php
@@ -1,14 +1,13 @@
 <html> 
-    <body> 
-        <?php 
-        $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
-        $cpumodel = pg_escape_string($_POST['cpumodel']); 
-        $cputypedescription = pg_escape_string($_POST['cputypedescription']); 
-        $cpucores = pg_escape_string($_POST['cpucores']);
-	$cpuvendor = pg_escape_string($_POST['cpuvendor']);
-        pg_query($db, "INSERT INTO cmd_cpu_type(vend_id, cpu_type_model, cpu_type_description, cpu_type_cores) VALUES('$cpuvendor', '$cpumodel', '$cputypedescription', '$cpucores')"); 
-	echo "The CPU Type was added successfully with values: <br />" . "CPU Model Number: " . $cpumodel . "<br />" . "CPU Name: " . $cputypedescription . "<br />" . "CPU Cores: " .  $cpucores . "<br />" . "CPU Vendor: " . $cpuvendor . "<br />"; 
-        pg_close(); 
-        ?> 
-    </body> 
+  <body> 
+    <?php 
+      $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
+      $cpu_type_model = pg_escape_string($_POST['cpu_type_model']); 
+      $cpu_type_description = pg_escape_string($_POST['cpu_type_description']); 
+      $cpu_type_cores = pg_escape_string($_POST['cpu_type_cores']);
+      $vend_id = pg_escape_string($_POST['vend_id']);
+      pg_query($db, "INSERT INTO cmd_cpu_type(vend_id, cpu_type_model, cpu_type_description, cpu_type_cores) VALUES('$vend_id', '$cpu_type_model', '$cpu_type_description', '$cpu_type_cores')"); 
+      echo "The CPU Type was added successfully with values: <br />" . "CPU Model Number: " . $cpu_type_model . "<br />" . "CPU Name: " . $cpu_type_description . "<br />" . "CPU Cores: " .  $cpu_type_cores . "<br />" . "CPU Vendor: " . $vend_id . "<br />"; 
+    ?> 
+  </body> 
 </html> 
