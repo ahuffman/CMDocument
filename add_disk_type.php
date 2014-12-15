5c8a46b554f95a7a5f46a71b87edd92f32a2574a
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-12-15 10:26:32 -0500

    Completed add action/handler. Updated variable names for more consistent code.

--- add_disk_type.php
+++ add_disk_type.php
@@ -3,10 +3,10 @@
         <?php 
         $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
 
-        $disktype = pg_escape_string($_POST['disktype']); 
-        $disktypedesc = pg_escape_string($_POST['disktypedesc']); 
-        pg_query($db, "INSERT INTO cmd_disk_type(disk_type, disk_type_desc) VALUES('$disktype', '$disktypedesc')"); 
-	echo "The object type was added successfully with values: <br />" . "Disk Type: " . $disktype . "<br />" . "Disk Type Description: " . $disktypedesc . "<br />"; 
+        $disk_type = pg_escape_string($_POST['disktype']); 
+        $disk_type_desc = pg_escape_string($_POST['disktypedesc']); 
+        pg_query($db, "INSERT INTO cmd_disk_type(disk_type, disk_type_desc) VALUES('$disk_type', '$disk_type_desc')"); 
+	echo "The object type was added successfully with values: <br />" . "Disk Type: " . $disk_type . "<br />" . "Disk Type Description: " . $disk_type_desc . "<br />"; 
         pg_close(); 
         ?> 
     </body> 
