e61b7cf8e1dbd9d477e2bfda6c8629c6811c9127
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-12-15 11:41:46 -0500

    Created add actions for object_type.

--- add_object_type.php
+++ add_object_type.php
@@ -1,13 +1,11 @@
 <html> 
-    <body> 
-        <?php 
-        $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
-
-        $objecttype = pg_escape_string($_POST['objecttype']); 
-        $objecttypedesc = pg_escape_string($_POST['objecttypedesc']); 
-        pg_query($db, "INSERT INTO cmd_object_type(obj_type, obj_type_desc) VALUES('$objecttype', '$objecttypedesc')"); 
-	echo "The object type was added successfully with values: <br />" . "Object Type: " . $objecttype . "<br />" . "Object Type Description: " . $objecttypedesc . "<br />"; 
-        pg_close(); 
-        ?> 
-    </body> 
+  <body> 
+    <?php 
+      $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
+      $obj_type = pg_escape_string($_POST['obj_type']); 
+      $obj_type_desc = pg_escape_string($_POST['obj_type_desc']); 
+      pg_query($db, "INSERT INTO cmd_object_type(obj_type, obj_type_desc) VALUES('$obj_type', '$obj_type_desc')"); 
+      echo "The object type was added successfully with values: <br />" . "Object Type: " . $obj_type . "<br />" . "Object Type Description: " . $obj_type_desc . "<br />"; 
+    ?> 
+  </body> 
 </html> 
