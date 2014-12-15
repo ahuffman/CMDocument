9253f1cc40bdffe74f4dcc70b85be0f217349941
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-12-15 15:43:31 -0500

    Created add actions for software.

--- add_sw.php
+++ add_sw.php
@@ -1,15 +1,13 @@
 <html> 
-    <body> 
-        <?php 
-        $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
-        
-        $vend = pg_escape_string($_POST['vend']);
-        $sw = pg_escape_string($_POST['sw']); 
-        $sw_ver = pg_escape_string($_POST['sw_ver']);
-        $arch = pg_escape_string($_POST['arch']);
-        pg_query($db, "INSERT INTO cmd_software(software,arch_id,sw_ver,vend_id) VALUES('$sw', '$arch', '$sw_ver', '$vend')"); 
-	echo 'The Software was added successfully with values: <br />' . 'Software Vendor: ' . $vend . '<br />' . 'Software Name: ' . $sw . '<br />' . 'Software Version: ' . $sw_ver . '<br />' . 'Software Architecture: ' . $arch . '<br />';
-        pg_close(); 
-        ?> 
-    </body> 
+  <body> 
+    <?php 
+      $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
+      $vend_id = pg_escape_string($_POST['vend_id']);
+      $software = pg_escape_string($_POST['software']); 
+      $sw_ver = pg_escape_string($_POST['sw_ver']);
+      $arch_id = pg_escape_string($_POST['arch_id']);
+      pg_query($db, "INSERT INTO cmd_software(software,arch_id,sw_ver,vend_id) VALUES('$software', '$arch_id', '$sw_ver', '$vend_id')"); 
+      echo 'The Software was added successfully with values: <br />' . 'Software Vendor: ' . $vend_id . '<br />' . 'Software Name: ' . $software . '<br />' . 'Software Version: ' . $sw_ver . '<br />' . 'Software Architecture: ' . $arch_id . '<br />';
+    ?> 
+  </body> 
 </html> 
