0ac36cddf0cb40e7c42a4c039ace07078426789b
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-12-15 12:06:23 -0500

    Created the add actions for models.

--- add_model.php
+++ add_model.php
@@ -3,10 +3,10 @@
         <?php 
         $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
         $model = pg_escape_string($_POST['model']);
-        $vendor = pg_escape_string($_POST['vendor']);
-        $type = pg_escape_string($_POST['type']);        
-        pg_query($db, "INSERT INTO cmd_model(model, vend_id, obj_type_id) VALUES('$model', '$vendor', '$type')"); 
-	echo "The Model was added successfully with values: <br />" . "Model: " . $model . "<br />" . "Vendor ID: " . $vendor . "<br />" . "Model Type: " . $type . "<br />";
+        $vend_id = pg_escape_string($_POST['vend_id']);
+        $obj_type_id = pg_escape_string($_POST['obj_type_id']);        
+        pg_query($db, "INSERT INTO cmd_model(model, vend_id, obj_type_id) VALUES('$model', '$vend_id', '$obj_type_id')"); 
+	echo "The Model was added successfully with values: <br />" . "Model: " . $model . "<br />" . "Vendor ID: " . $vend_id . "<br />" . "Model Type: " . $obj_type_id . "<br />";
         pg_close(); 
         ?> 
     </body> 
