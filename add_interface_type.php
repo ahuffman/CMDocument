108e8f5e1dbf66f9831b8857537e2c79356fa8b3
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-12-15 11:19:27 -0500

    Created add action for interface_type.

--- add_interface_type.php
+++ add_interface_type.php
@@ -2,11 +2,10 @@
     <body> 
         <?php 
         $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
-
-        $inttype = pg_escape_string($_POST['inttype']); 
-        $inttypedesc = pg_escape_string($_POST['inttypedesc']); 
-        pg_query($db, "INSERT INTO cmd_interface_type(int_type, int_type_desc) VALUES('$inttype', '$inttypedesc')"); 
-	echo "The interface type was added successfully with values: <br />" . "Interface Type: " . $inttype . "<br />" . "Interface Type Description: " . $inttypedesc . "<br />"; 
+        $int_type = pg_escape_string($_POST['int_type']); 
+        $int_type_desc = pg_escape_string($_POST['int_type_desc']); 
+        pg_query($db, "INSERT INTO cmd_interface_type(int_type, int_type_desc) VALUES('$int_type', '$int_type_desc')"); 
+	echo "The interface type was added successfully with values: <br />" . "Interface Type: " . $int_type . "<br />" . "Interface Type Description: " . $int_type_desc . "<br />"; 
         pg_close(); 
         ?> 
     </body> 
