a9fe6d302baf91c214ba085b66f9bb4c03b2d6a6
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-12-17 09:35:23 -0500

    Created add action for raid_types.

--- add_raid_type.php
+++ add_raid_type.php
@@ -1,12 +1,10 @@
 <html> 
-    <body> 
-        <?php 
-        $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
-
-        $raidtype = pg_escape_string($_POST['raidtype']); 
-        pg_query($db, "INSERT INTO cmd_disk_raid_type(disk_raid_type) VALUES('$raidtype')"); 
-	echo "The RAID type was added successfully with values: <br />" . "RAID Type: " . $raidtype . "<br />"; 
-        pg_close(); 
-        ?> 
-    </body> 
+  <body> 
+    <?php 
+      $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
+      $disk_raid_type = pg_escape_string($_POST['disk_raid_type']); 
+      pg_query($db, "INSERT INTO cmd_disk_raid_type(disk_raid_type) VALUES('$disk_raid_type')"); 
+      echo "The RAID type was added successfully with values: <br />" . "RAID Type: " . $disk_raid_type . "<br />"; 
+    ?> 
+  </body> 
 </html> 
