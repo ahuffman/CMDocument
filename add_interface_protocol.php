00dab019a0edcbd23e5dd901cb8390febc8609b1
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-12-17 09:41:59 -0500

    Created add actions for interface_protocol.

--- add_interface_protocol.php
+++ add_interface_protocol.php
@@ -1,12 +1,10 @@
 <html> 
-    <body> 
-        <?php 
-        $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
-
-        $intproto = pg_escape_string($_POST['intproto']); 
-        pg_query($db, "INSERT INTO cmd_interface_protocol(int_proto) VALUES('$intproto')"); 
-	echo "The Interface Protocol was added successfully with values: <br />" . "Interface Protocol: " . $intproto . "<br />";
-        pg_close(); 
-        ?> 
-    </body> 
+  <body> 
+    <?php 
+      $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
+      $int_proto = pg_escape_string($_POST['int_proto']); 
+      pg_query($db, "INSERT INTO cmd_interface_protocol(int_proto) VALUES('$int_proto')"); 
+      echo "The Interface Protocol was added successfully with values: <br />" . "Interface Protocol: " . $int_proto . "<br />";
+    ?> 
+  </body> 
 </html> 
