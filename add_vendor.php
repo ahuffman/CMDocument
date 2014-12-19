06d1082fc4396228a3b64d402f22f0585148ab41
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-12-19 11:23:01 -0500

    Created add actions for vendor.

--- add_vendor.php
+++ add_vendor.php
@@ -1,12 +1,10 @@
 <html> 
-    <body> 
-        <?php 
-        $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
-
-        $vendor = pg_escape_string($_POST['vendor']); 
-        pg_query($db, "INSERT INTO cmd_vendors(vend) VALUES('$vendor')"); 
-	echo "The Vendor was added successfully with values: <br />" . "Vendor Name: " . $vendor . "<br />"; 
-        pg_close(); 
-        ?> 
-    </body> 
+  <body> 
+    <?php 
+      $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
+      $vend = pg_escape_string($_POST['vend']); 
+      pg_query($db, "INSERT INTO cmd_vendors(vend) VALUES('$vend')"); 
+      echo "The Vendor was added successfully with values: <br />" . "Vendor Name: " . $vend . "<br />"; 
+    ?> 
+  </body> 
 </html> 
