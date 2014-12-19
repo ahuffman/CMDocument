f34def768701b6662dbfd0e39d66be44ef32ac03
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-12-19 10:12:15 -0500

    Created add actions for cabinet.

--- add_cabinet.php
+++ add_cabinet.php
@@ -1,13 +1,12 @@
 <html> 
-    <body> 
-        <?php 
-        $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
-        $cabname = pg_escape_string($_POST['cabname']); 
-        $cabrus = pg_escape_string($_POST['cabrus']); 
-        $location = pg_escape_string($_POST['location']);
-        pg_query($db, "INSERT INTO cmd_cabinets(location_id, cab_name, cab_units_tot) VALUES('$location', '$cabname', '$cabrus')"); 
-	echo "The Cabinet was added successfully with values: <br />" . "Cabinet Name: " . $cabname . "<br />" . "Cabinet Total Rack Units: " . $cabrus . "<br />" . "Cabinet Location ID: " .  $location . "<br />"; 
-        pg_close(); 
-        ?> 
-    </body> 
+  <body> 
+    <?php 
+      $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
+      $cab_name = pg_escape_string($_POST['cab_name']); 
+      $cab_units_tot = pg_escape_string($_POST['cab_units_tot']); 
+      $location_id = pg_escape_string($_POST['location_id']);
+      pg_query($db, "INSERT INTO cmd_cabinets(location_id, cab_name, cab_units_tot) VALUES('$location_id', '$cab_name', '$cab_units_tot')"); 
+      echo "The Cabinet was added successfully with values: <br />" . "Cabinet Name: " . $cab_name . "<br />" . "Cabinet Total Rack Units: " . $cab_units_tot . "<br />" . "Cabinet Location ID: " .  $location_id . "<br />"; 
+    ?> 
+  </body> 
 </html> 
