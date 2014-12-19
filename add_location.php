90dfc0af23fe3493eb2a8fa151c0ee03a450726a
Andrew Huffman <ahuffman@users.noreply.github.com>
2014-12-19 10:11:46 -0500

    Created add actions for location.

--- add_location.php
+++ add_location.php
@@ -1,13 +1,11 @@
 <html> 
-    <body> 
-        <?php 
-        $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
-
-        $locname = pg_escape_string($_POST['locationname']);
-        $locaddr = pg_escape_string($_POST['locationaddress']); 
-        pg_query($db, "INSERT INTO cmd_locations(location_name, location_address) VALUES('$locname', '$locaddr')"); 
-	echo "The Location was added successfully with values: <br />" . "Location Name: " . $locname . "<br />" . "Location Address: " . $locaddr . "<br />";
-        pg_close(); 
-        ?> 
-    </body> 
+  <body> 
+    <?php 
+      $db = pg_connect("host=usfr-itdocument.insideidc.com dbname=cmdocument user=cmdocument password=h0twheels") or die('Could not connect');
+      $location_name = pg_escape_string($_POST['location_name']);
+      $location_address = pg_escape_string($_POST['location_address']); 
+      pg_query($db, "INSERT INTO cmd_locations(location_name, location_address) VALUES('$location_name', '$location_address')"); 
+      echo "The Location was added successfully with values: <br />" . "Location Name: " . $location_name . "<br />" . "Location Address: " . $location_address . "<br />";
+    ?> 
+  </body> 
 </html> 
