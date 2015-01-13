f6e5bf684d5bf4647c4914ae517d473a2b2333c6
Andrew Huffman <ahuffman@users.noreply.github.com>
2015-01-13 10:51:23 -0500

    Register and login fully working.  Need to add session work to login.php still.

--- login.php
+++ login.php
@@ -1,63 +1,50 @@
 <html>
-<body>
-<?php
-    $config = parse_ini_file("./cmdocument.ini.php",false);
-    $user = $config['user'];
-    $pass = $config['pass'];
-    $dbname = $config['dbname'];
-    $host = $config['hostname'];
-    $db = pg_connect('host=' . $host . ' dbname=' . $dbname . ' user=' . $user . ' password=' . $pass) or die('Could not connect');
-    if (isset($_POST['referer'])) {$ref = pg_escape_string($_POST['referer']);}
-    if (isset($_POST['user_login'])) {$user_login = pg_escape_string($_POST['user_login']);}
-    if (isset($_POST['user_pass'])) {$user_pass = pg_escape_string($_POST['user_pass']);}
-    if (!isset($_POST['referer'])) {
-      echo '  <form action="login.php" id="login" method="post">' . "\r\n" .
-      '    Username:' . "\r\n" .
-      '    <input type="text" name="user_login">' . '<br />' . "\r\n" .
-      '    Password:' . "\r\n" .
-      '    <input type="password" name="user_pass">' . '<br />' . "\r\n" .
-      '    <input type="hidden" name="referer" value="login.php">' . "\r\n" .
-      '    <input type="submit" name="submit" value="Login">' . "\r\n" .
-      '    <input type="reset" name="reset" value="Clear">' . "\r\n" .
-      '  </form>' . "\r\n";
-    }
-    else if ($ref=="login.php") {
-      $qry_user = pg_query($db, "SELECT user_pass, user_salt FROM cmd_users WHERE user_login='$user_login'");
-      while ($userrow = pg_fetch_assoc($qry_user)) {
-        $hash = hash(sha256,$userrow['user_salt'] + $user_pass);
-        if ($hash == $userrow['user_pass']) {
-          //start working on a sesison
-          echo 'Login Check Passed';
-            //debug
-              //echo 'Trying user: ' . $user_login . '<br />';
-              //echo 'USER SALT: ' . $userrow['user_salt'] . '<br />';
-              //echo 'USER PASS: ' . $userrow['user_pass'] . '<br />';
-              //echo 'login attempt: ' . $hash . '<br />';
-
-//            session_start();
-//            if(isset($_SESSION['use'])) {
-//              header("location:set_gen.html");
-//              echo 'Already Logged In.' . '<br />' . "\r\n";
-//            }
-//            else {
-//              session_start();
-//              $_SESSION['login_user']= $user_login;
-//              echo 'Session started:' . '<br />' .
-//              $_SESSION['login_user'];
-//              echo '<form id="logout" method="post">' . "\r\n" . 
-//              '  <input type="button" name="logout" value="Logout">' . "\r\n"; //add session destroy code: session_destroy()
-//            }
+  <body>
+    <?php
+      $config = parse_ini_file("./cmdocument.ini.php",false);
+      $user = $config['user'];
+      $pass = $config['pass'];
+      $dbname = $config['dbname'];
+      $host = $config['hostname'];
+      $db = pg_connect('host=' . $host . ' dbname=' . $dbname . ' user=' . $user . ' password=' . $pass) or die('Could not connect');
+      if (isset($_POST['referer'])) {$ref = pg_escape_string($_POST['referer']);}
+//      if (isset($_POST['user_login'])) {$user_login = pg_escape_string($_POST['user_login']);}
+//      if (isset($_POST['user_pass'])) {$user_pass = pg_escape_string($_POST['user_pass']);}
+$user_pass = '';
+$user_login = '';
+      if (!isset($_POST['referer'])) {
+        echo '  <form action="login.php" id="login" method="POST">' . "\r\n" .
+        '    Username:' . "\r\n" .
+        '    <input type="text" name="user_login">' . '<br />' . "\r\n" .
+        '    Password:' . "\r\n" .
+        '    <input type="password" name="user_pass">' . '<br />' . "\r\n" .
+        '    <input type="hidden" name="referer" value="login.php">' . "\r\n" .
+        '    <input type="submit" name="submit" value="Login">' . "\r\n" .
+        '    <input type="reset" name="reset" value="Clear">' . "\r\n" .
+        '  </form>' . "\r\n";
+      }
+      else if ($ref=="login.php") {
+        if (isset($_POST['user_login'])) {$user_login = pg_escape_string($_POST['user_login']);}
+        if (isset($_POST['user_pass'])) {$user_pass = pg_escape_string($_POST['user_pass']);}
+        $qry_user = pg_query($db, "SELECT * FROM cmd_users WHERE user_login='$user_login'");
+        if (pg_num_rows($qry_user) <= 0) {
+          //user entered a username not registered in the db
+          echo 'Bad username or password.' . '<br />' . "\r\n";
+          header('Refresh: 3; URL=login.php');
         }
         else {
-          echo 'Login Check Failed.' . '<br />';
-            //debug
-              //echo 'Trying user: ' . $user_login . '<br />';
-              //echo 'USER SALT: ' . $userrow['user_salt'] . '<br />';
-              //echo 'USER PASS: ' . $userrow['user_pass'] . '<br />';
-              //echo 'login attempt: ' . $hash . '<br />';
+          $userrow = pg_fetch_assoc($qry_user); 
+          $saltpass = $userrow['user_salt'] . $user_pass;
+          $hash = hash('sha256',"$saltpass");
+          if ($hash == $userrow['user_pass']) {
+            //start working on a sesison
+            echo 'Login Check Passed';
+          }
+          else {
+            echo 'Login Check Failed.' . '<br />';
+          }
         }
       }
-    }
-?>
-</body>
+    ?>
+  </body>
 </html>
--- register.php
+++ register.php
@@ -1,26 +1,70 @@
 <html>
   <body>
     <?php
+      $config = parse_ini_file("./cmdocument.ini.php",false);
+      $dbuser = $config['user'];
+      $dbpass = $config['pass'];
+      $dbname = $config['dbname'];
+      $host = $config['hostname'];
+      $db = pg_connect('host=' . $host . ' dbname=' . $dbname . ' user=' . $dbuser . ' password=' . $dbpass) or die('Could not connect');
       if(isset($_POST['referer'])){$referer = pg_escape_string($_POST['referer']);}
       if ($referer=="register.php") {
         if(isset($_POST['user_date'])) {
           $date = $_POST['user_date'];
           $salt = hash(sha256,"$date"); //creating random salt based off of the date we're attempting to register - to be inserted to user table
         }
-        if(isset($_POST['user_login'])) {$user_login = pg_escape_string($_POST['user_login']);}
-        if(isset($_POST['user_firstname'])) {$user_firstname = pg_escape_string($_POST['user_firstname']);}
-        if(isset($_POST['user_lastname'])) {$user_lastname = pg_escape_string($_POST['user_lastname']);}
         $firstlogindate = str_replace(' ','',date("D M d, Y G:i")); //don't want a chance of having a reproducable salt from a temporary stored db value
+        //validate unique username
+        if(isset($_POST['user_login'])) {
+          $qry_users = pg_query($db, "SELECT user_login FROM cmd_users");
+          $user_login = strtolower(pg_escape_string($_POST['user_login']));
+          while($users = pg_fetch_assoc($qry_users)) {
+            if($user_login==$users['user_login']) {
+              echo 'Username already exists.  Please provide a unique username.' . '<br />' . "\r\n";
+              echo 'User Registration Failed.' . '<br />' . "\r\n";
+              header('Refresh: 3; URL=register.php');
+              exit();
+            }
+            //validate non-null username
+            else if($user_login=="") {
+              echo 'No username provided.' . '<br />' . "\r\n";
+              echo 'User Registration Failed.' . '<br />' . "\r\n";
+              header('Refresh: 3; URL=register.php');
+              exit();
+            }
+          }
+        }
         if(isset($_POST['user_pass'])) {
-          $config = parse_ini_file("./cmdocument.ini.php",false);
-          $dbuser = $config['user'];
-          $dbpass = $config['pass'];
-          $dbname = $config['dbname'];
-          $host = $config['hostname'];
-          $db = pg_connect('host=' . $host . ' dbname=' . $dbname . ' user=' . $dbuser . ' password=' . $dbpass) or die('Could not connect');
           $ip = $_SERVER['REMOTE_ADDR'];
           $pass = pg_escape_string($_POST['user_pass']);
-          $hash = hash(sha256,"$salt" + "$pass"); //creating salted hashed password - to be inserted to user table
+          //validate we have a non-null password
+          if ($pass=="") {
+            echo 'No password provided.' . '<br />' . "\r\n";
+            echo 'User Registration Failed.' . '<br />' . "\r\n";
+            header('Refresh: 3; URL=register.php');
+            exit();
+          }
+        if(isset($_POST['user_firstname'])) {
+          $user_firstname = pg_escape_string($_POST['user_firstname']);
+          //validate we have a non-null firstname
+          if($user_firstname=="") {
+            echo 'No user firstname provided.' . '<br />' . "\r\n";
+            echo 'User Registration Failed.' . '<br />' . "\r\n";
+            header('Refresh: 3; URL=register.php');
+            exit();
+          }
+        }
+        if(isset($_POST['user_lastname'])) {
+          $user_lastname = pg_escape_string($_POST['user_lastname']);
+          if($user_lastname=="") {
+            echo 'No user lastname provided.' . '<br />' . "\r\n";
+            echo 'User Registration Failed.' . '<br />' . "\r\n";
+            header('Refresh: 3; URL=register.php');
+            exit();
+          }
+        }
+          $saltpass = $salt . $pass;
+          $hash = hash('sha256',"$saltpass"); //creating salted hashed password - to be inserted to user table
           $qry_register = pg_query($db, "INSERT INTO cmd_users(user_login, user_pass, user_salt, user_firstname, user_lastname, user_ip, user_session, user_lastlogin, user_loginfail) VALUES('$user_login', '$hash', '$salt', '$user_firstname', '$user_lastname', '$ip', 'x', '$firstlogindate', '0')");
           if(!$qry_register) {
             echo 'Registration failed due to database error.';
@@ -30,11 +74,6 @@
             header('Refresh: 3; URL=login.php');
           }
         }
-        else {
-          echo 'Password not provided.';
-          //redirect to registration page
-          header("location:register.php");          
-        } 
       }
       else {
         $date = str_replace(' ','',date("D M d, Y G:i")); //creates random salt off of the date string to harden password
