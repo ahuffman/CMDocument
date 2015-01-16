ccbd3a17a20f72168307582eab5b7f74703d4813
Andrew Huffman <ahuffman@users.noreply.github.com>
2015-01-16 15:01:16 -0500

    Fixed bug in vendor_id post name

--- del_sw.php
+++ del_sw.php
@@ -5,7 +5,7 @@
       if (isset($_POST['sw_id'])) {$sw_id = pg_escape_string($_POST['sw_id']);} else {echo 'No value provided: software id.'; exit();}
       if (isset($_POST['software'])) {$software = pg_escape_string($_POST['software']);} else {echo 'No value provided: software.'; exit();}
       if (isset($_POST['sw_ver'])) {$sw_ver = pg_escape_string($_POST['sw_ver']);} else {echo 'No value provided: software version.'; exit();}
-      if (isset($_POST['vend'])) {$vend = pg_escape_string($_POST['vend']);} else {echo 'No value provided: vendor.'; exit();}
+      if (isset($_POST['vend_id'])) {$vend_id = pg_escape_string($_POST['vend_id']);} else {echo 'No value provided: vendor id.'; exit();}
       if (isset($_POST['arch'])) {$arch = pg_escape_string($_POST['arch']);} else {echo 'No value provided: architecture.'; exit();}
       if (isset($_POST['referer'])) {$ref = pg_escape_string($_POST['referer']);} else {echo 'No value provided: referer.'; exit();}
       if (isset($ref)) {
