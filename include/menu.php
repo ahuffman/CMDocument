71cbe43c6d455bc2d3f8559a65fa2605dc1dc91f
Andrew Huffman <ahuffman@users.noreply.github.com>
2015-01-16 13:24:14 -0500

    added more security, and special checks for login.php condition after implementing msg_display_time and realizing I needed to include menu.php on the login.php page.

--- include/menu.php
+++ include/menu.php
@@ -8,13 +8,17 @@
   $dbhost = $config['hostname'];
   $db = pg_connect('host=' . $dbhost . ' dbname=' . $dbname . ' user=' . $dbuser . ' password=' . $dbpass) or die('Could not connect');
   $msg_display_time = $config['msg_display_time'];
+  $check_page = fnmatch('*login.php', $_SERVER['SCRIPT_NAME']);
   //start user session
   session_start();
   session_write_close();
-  if (!isset($_SESSION['user_login'])) {
-    echo 'You must login first.';
-    header("Refresh: $msg_display_time; URL=login.php");
-    exit();
+  if (!$check_page) { 
+    //we're not on login page change to login.php if no session - prevent permanent loop
+    if (!isset($_SESSION['user_login'])) {
+      echo 'You must login first.';
+      header("Refresh: $msg_display_time; URL=login.php");
+      exit();
+    }
   }
               //debug
 //                foreach ($_SESSION as $key=>$val)
@@ -27,9 +31,12 @@
        '    </td>' . "\r\n" .
        '  </tr>' . "\r\n" .
        '  <tr>' . "\r\n" .
-       '    <td>' . "\r\n" .
-       '      <a href="' . $url . '/logout.php">Logout: ' . $_SESSION['user_login'] . '</a>' . "\r\n" .
-       '    </td>' . "\r\n" .
+       '    <td>' . "\r\n";
+  if (!$check_page) {
+    //not the login page so show user the logout link
+    echo '      <a href="' . $url . '/logout.php">Logout: ' . $_SESSION['user_login'] . '</a>' . "\r\n";
+  }
+  echo '    </td>' . "\r\n" .
        '  </tr>' . "\r\n" .
        '</table>' . "\r\n";
 ?>
--- login.php
+++ login.php
@@ -1,12 +1,7 @@
 <html>
   <body>
     <?php
-      $config = parse_ini_file("./cmdocument.ini.php",false);
-      $user = $config['user'];
-      $pass = $config['pass'];
-      $dbname = $config['dbname'];
-      $host = $config['hostname'];
-      $db = pg_connect('host=' . $host . ' dbname=' . $dbname . ' user=' . $user . ' password=' . $pass) or die('Could not connect');
+      include './include/menu.php';
       $user_pass = '';
       $user_login = '';
       if (isset($_POST['referer'])) {$ref = pg_escape_string($_POST['referer']);}
