d1c3781e73f6a88efce34ae3bb9617be7beb5a28
Andrew Huffman <ahuffman@users.noreply.github.com>
2015-02-24 15:56:36 -0500

    adding some CSS to the account page.

--- account.php
+++ account.php
@@ -2,10 +2,11 @@
   <body>
     <?php
       include './include/menu.php';
+      echo '<div class="account_main">' . "\r\n";
       $qry_account = pg_query($db, "SELECT user_firstname, user_lastname, user_ip, user_lastlogin FROM cmd_users WHERE user_login='" . $_SESSION['user_login'] . "'");
       if (!$qry_account) {
         //query failed
-        echo 'Bad query';
+        showMessage('message','Unknown username or database error.');
       }
       else {
         while ($account = pg_fetch_assoc($qry_account)) {
@@ -34,7 +35,8 @@
                '    <td>' . '<a href="edt_account.php">' .  'Edit Account' . '</a>' . '</td>' . "\r\n" .
                '    <td>' . '<a href="edt_pw.php">' . 'Change Password' . '</a>' . '</td>' . "\r\n" .
                '  </tr>' . "\r\n" .
-               '</table>' . "\r\n";
+               '</table>' . "\r\n" .
+               '</div>' . "\r\n";
         }
       }
     ?>
